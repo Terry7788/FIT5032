3a7e7d8de7fb3b3c876df19d08dd2446e1d198d2
Terry <terry.manh.truong@gmail.com>
2024-07-25 13:28:23 +1000

    Week 1 Efolio
    Added Week 1 EFolio Tasks

---
+++ Week 1/vue-project/src/components/MyName.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="blank-page">
+    <h1 >My Name is Terry Truong</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+};
+</script>
+
+<style scoped>
+.blank-page {
+  background-color: white;
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+h1 {
+    color: black; /* Add this line to make the text black */
+  }
+</style>
