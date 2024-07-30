d0a4571988d54c2dd112ee95c766436b8ba19654
Terry <terry.manh.truong@gmail.com>
2024-07-30 22:00:15 +1000

    Completed Efolio task 2.1 and 2.2

--- Week 2/ttruong-library/src/App.vue
+++ Week 2/ttruong-library/src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <JSON></JSON>
+      
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
+
+
 </template>
 
 <style scoped>
